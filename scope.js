961ca816d63453745842a176b7bc876c81e9c25c
ashikprodhan <abubakarsiddik652@gmail.com>
2021-02-03 14:37:16 +0800

    Closer slice splice join scope

---
+++ scope.js
@@ -0,0 +1,18 @@
+let bonus =5;
+function sum(first ,second) {
+    let result =first + second +bonus;
+    // return result ;
+    if (result > 5) {
+        var mood ="happy";
+        mood= 'more happy';
+        console.log(mood);
+        
+    }
+    
+    var day ="friday";
+    console.log(day);
+    return result;
+   
+}
+const outPut =sum(5,6);
+console.log(outPut);
---
+++ splice.js
@@ -0,0 +1,4 @@
+const number=[1,2,3,4,5,6,7,8,9];
+const removed =number.splice(2,2);// it will remove two element from index 2
+console.log(removed);
+console.log(number);// array will be updated because of splice function 
